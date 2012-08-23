e93c779a9fe6ef9bfab3a072b5769e793d67b165
Hannes Kirsman <hannes@mekaia.ee>
2012-08-23 11:06:32 +0300

    Cleanup
    Change mockup and overlay definition to 'web composition'. For example "Please
    upload new web composition!
    Also replaced overlay with composition in all code.
    
    Remove code that restricts having word 'test' in a file name.

--- perfecto.js
+++ perfecto.js
@@ -1,11 +1,10 @@
-// Perfecto
 (function($) {
   $(document).ready(function() {
 
-    var overlay_panel_url = Drupal.settings.basePath + 'admin/config/development/perfecto/overlay_panel';
+    var composition_panel_url = Drupal.settings.basePath + 'admin/settings/perfecto/composition_control_panel';
 
     $.ajax({
-      url: overlay_panel_url,
+      url: composition_panel_url,
       success: function(data) {
         $('body').append(data);
         init_perfecto();
@@ -41,27 +40,27 @@
       }
 
       /**
-       * Show design overlay on page
+       * Show design composition on page
        */
       (function() {
-          //var file = gup('overlay');
+          //var file = gup('composition');
 
           var
-          imageoverlay_wrap = $('#perfecto__imageoverlaycontrols_wrap'), // the image over
-          imageoverlaycontrols = $('#perfecto__imageoverlaycontrols'),
-          imageoverlaycontrols_mousehook = $('#perfecto__imageoverlaycontrols_mousehook'),
+          imagecomposition_wrap = $('#perfecto__imagecompositioncontrols_wrap'), // the image over
+          imagecompositioncontrols = $('#perfecto__imagecompositioncontrols'),
+          imagecompositioncontrols_mousehook = $('#perfecto__imagecompositioncontrols_mousehook'),
           page = (document.location+'').split('/')[4].replace('.html', '').split('?')[0],
-          opacity = $.cookie('perfecto__imageoverlaycontrols_opacity')?$.cookie('perfecto__imageoverlaycontrols_opacity'):0.4,
-          top = $.cookie('overlay_position_y')?$.cookie('overlay_position_y'):0,
-          left = $.cookie('overlay_position_x')?$.cookie('overlay_position_x'):0,
-          filename = $.cookie('overlay_filename')?$.cookie('overlay_filename'):$('#perfecto__imageoverlaycontrols-files option:first').val(),
+          opacity = $.cookie('perfecto__imagecompositioncontrols_opacity')?$.cookie('perfecto__imagecompositioncontrols_opacity'):0.4,
+          top = $.cookie('composition_position_y')?$.cookie('composition_position_y'):0,
+          left = $.cookie('composition_position_x')?$.cookie('composition_position_x'):0,
+          filename = $.cookie('composition_filename')?$.cookie('composition_filename'):$('#perfecto__imagecompositioncontrols-files option:first').val(),
           lock = false,
           dragging = false,
-          overlay_position_x,
-          overlay_position_y,
+          composition_position_x,
+          composition_position_y,
           drag_start_x,
           drag_start_y,
-          overlay_visible = $.cookie('perfecto__imageoverlaycontrols_visible')?$.cookie('perfecto__imageoverlaycontrols_visible'):'false',
+          composition_visible = $.cookie('perfecto__imagecompositioncontrols_visible')?$.cookie('perfecto__imagecompositioncontrols_visible'):'false',
           move10px = false;
 
           /*if (file.length==0) {
@@ -70,26 +69,26 @@
           }
           */
 
-          $('#perfecto__imageoverlaycontrols-files').change(function() {
+          $('#perfecto__imagecompositioncontrols-files').change(function() {
             filename = $(this).find('option:selected').val();
             filename_full = Drupal.settings.basePath + 'sites/default/files/mod_perfecto/unmanaged/' + filename;
-            overlay.attr('src', filename_full);
-            $.cookie('overlay_filename', filename);
+            composition.attr('src', filename_full);
+            $.cookie('composition_filename', filename);
           });
 
           var filename_full = Drupal.settings.basePath + 'sites/default/files/mod_perfecto/unmanaged/' + filename;
 
-          var overlay = $('<img id="perfecto__imageoverlaycontrols_img" src="' + filename_full + '" alt="" />');
-          overlay.css({
+          var composition = $('<img id="perfecto__imagecompositioncontrols_img" src="' + filename_full + '" alt="" />');
+          composition.css({
               "position": "absolute",
               "z-index" : -99999,
               "top"     : top+"px",
               "left"     : left+"px",
               "opacity" : opacity,
-              "display" : overlay_visible==='true'?'block':'none'
+              "display" : composition_visible==='true'?'block':'none'
           });
 
-          // drag the overlay via small div under cursor
+          // drag the composition via small div under cursor
           var draggable_handle = $('<div id="perfecto_draggable_handle"></div>');
           draggable_handle.css("position", "absolute"); // chrome fix
           draggable_handle.draggable({
@@ -97,23 +96,23 @@
                   drag_start_y = ui.position.top;
                   drag_start_x = ui.position.left;
 
-                  var offset_overlay = overlay.offset();
+                  var offset_composition = composition.offset();
                   var offset_draggable_handle = draggable_handle.offset();
-                  overlay_position_x = offset_overlay.left;
-                  overlay_position_y = offset_overlay.top;
+                  composition_position_x = offset_composition.left;
+                  composition_position_y = offset_composition.top;
                   draggable_handle_drag_start_x = offset_draggable_handle.top;
                   draggable_handle_drag_start_y = offset_draggable_handle.left;
 
                   dragging = true;
               },
               stop: function(e, ui) {
-                  var offset_overlay = overlay.offset();
+                  var offset_composition = composition.offset();
                   dragging = false;
-                  overlay_position_x = offset_overlay.left;
-                  overlay_position_y = offset_overlay.top;
+                  composition_position_x = offset_composition.left;
+                  composition_position_y = offset_composition.top;
 
-                  $.cookie("overlay_position_y", overlay_position_y);
-                  $.cookie("overlay_position_x", overlay_position_x);
+                  $.cookie("composition_position_y", composition_position_y);
+                  $.cookie("composition_position_x", composition_position_x);
               },
               drag: function(e, ui) {
                   if (lock || !e.ctrlKey) {
@@ -122,72 +121,72 @@
                   top = ui.position.top;
                   left = ui.position.left;
 
-                  overlay.css({
-                      "top": overlay_position_y + top - drag_start_y+"px",
-                      "left": overlay_position_x + left - drag_start_x +"px"
+                  composition.css({
+                      "top": composition_position_y + top - drag_start_y+"px",
+                      "left": composition_position_x + left - drag_start_x +"px"
                   });
 
                   dragging = true;
               }
           });
 
-          $( "#perfecto__imageoverlaycontrols-opacity-slider" ).slider({
+          $( "#perfecto__imagecompositioncontrols-opacity-slider" ).slider({
               value:opacity,
               min: 0,
               max: 1,
               step: 0.1,
               slide: function( event, ui ) {
-                  $.cookie("perfecto__imageoverlaycontrols_opacity", ui.value);
+                  $.cookie("perfecto__imagecompositioncontrols_opacity", ui.value);
 
-                  //$("#overlayOpacity").val(ui.value);
+                  //$("#compositionOpacity").val(ui.value);
                   perfecto__body.css({
                       "opacity": ui.value
                   });
               }
           });
 
-          perfecto__body.after(imageoverlay_wrap);
-          $('#perfecto__imageoverlaycontrols').disableSelection();
+          perfecto__body.after(imagecomposition_wrap);
+          $('#perfecto__imagecompositioncontrols').disableSelection();
           perfecto__body.after(draggable_handle);
-          perfecto__body.after(overlay);
+          perfecto__body.after(composition);
 
           $("#resetToggle").click(function() {
               top = left = 0;
-              $.cookie("overlay_position_y", top);
-              $.cookie("overlay_position_x", left);
-              overlay.css({
+              $.cookie("composition_position_y", top);
+              $.cookie("composition_position_x", left);
+              composition.css({
                   "top"     : top+"px",
                   "left"    : left+"px"
               });
           });
 
-          if (overlay_visible==="true") {
+          if (composition_visible==="true") {
               perfecto__body.css({
                   "opacity": opacity
               });
           }
 
-          $("#perfecto__imageoverlaycontrols_toggle").click(function() {
-              if (overlay_visible==="true") {
-                  overlay_visible = "false";
+          $("#perfecto__imagecompositioncontrols_toggle").click(function() {
+              if (composition_visible==="true") {
+                  composition_visible = "false";
                   perfecto__body.css({
                       "opacity": 1
                   })
               } else {
-                  overlay_visible = "true";
+                  composition_visible = "true";
                   perfecto__body.css({
                       "opacity": opacity
                   })
               }
-              $.cookie("perfecto__imageoverlaycontrols_visible", overlay_visible);
+              $.cookie("perfecto__imagecompositioncontrols_visible", composition_visible);
 
               /*if (opacity != 0) {
                   opacity = 0;
               } else {
-                  opacity = $("#overlayOpacity").val();
+                  opacity = $("#compositionOpacity").val();
               }
               */
-              $("#perfecto__imageoverlaycontrols_img").toggle();
+              $("#perfecto__imagecompositioncontrols_img").toggle();
               $(this).blur();
           });
 
@@ -217,15 +216,15 @@
                       left += step;
                   }
 
-                  $.cookie("psdOverlayTop", top);
-                  $.cookie("psdOverlayLeft", left);
+                  $.cookie("psdcompositionTop", top);
+                  $.cookie("psdcompositionLeft", left);
 
                   if ( e.keyCode==38 || e.keyCode==40 ) { // up or down
-                      overlay.css({
+                      composition.css({
                           "top"     : top+"px"
                       });
                   } else if ( e.keyCode==37 || e.keyCode==39 ) {
-                      overlay.css({
+                      composition.css({
                           "left"     : left+"px"
                       });
                   }
@@ -234,21 +233,21 @@
           });
 
                */
-          $("#overlayFile").keyup( function(e) {
+          $("#compositionFile").keyup( function(e) {
               if (lock) {
                   return false;
               }
 
-              file =  $("#overlayFile").val();
-              $.cookie("psdOverlayFile", file);
+              file =  $("#compositionFile").val();
+              $.cookie("psdcompositionFile", file);
 
-              //$("#overlayImg").attr("src", "/"+site+"/overlays/"+file);
+              //$("#compositionImg").attr("src", "/"+site+"/compositions/"+file);
           });
 
           // This creates little draggable div under cursor when ctrl key is held
           // down. Firefox can't move it fast enough thoug.
           $(window).keydown( function(e) {
-              if (e.ctrlKey && !dragging && overlay_visible=='true' ) {
+              if (e.ctrlKey && !dragging && composition_visible=='true' ) {
                   draggable_handle.css({
                       "display" : "block",
                       "left": perfecto_mouse_x-25+"px",
@@ -268,7 +267,7 @@
           });
 
 
-          $("#overlayLock").click(function(e) {
+          $("#compositionLock").click(function(e) {
               if ( this.checked ) {
                   lock = true;
               } else {
@@ -276,19 +275,19 @@
               }
           });
 
-          // overlay move
-          var overlay_mover_jquery_selector = "#perfecto__xmover-left, "+
+          // composition move
+          var composition_mover_jquery_selector = "#perfecto__xmover-left, "+
           "#perfecto__xmover-right, "+
           "#perfecto__ymover-down, "+
           "#perfecto__ymover-up";
-          $(overlay_mover_jquery_selector).click(function(e) {
+          $(composition_mover_jquery_selector).click(function(e) {
 
               if (lock) {
                   return false;
               }
 
-              var overlay_position_y = $.cookie("overlay_position_y")*1.0;
-              var overlay_position_x = $.cookie("overlay_position_x")*1.0;
+              var composition_position_y = $.cookie("composition_position_y")*1.0;
+              var composition_position_x = $.cookie("composition_position_x")*1.0;
               var movestep;
               if ( move10px ) {
                   movestep = 10;
@@ -297,35 +296,35 @@
               }
 
               if (this.id=="perfecto__xmover-left") {
-                  overlay_position_x = overlay_position_x - movestep;
-                  $("#perfecto__imageoverlaycontrols-xmover-input").val(overlay_position_x);
+                  composition_position_x = composition_position_x - movestep;
+                  $("#perfecto__imagecompositioncontrols-xmover-input").val(composition_position_x);
               } else if (this.id=="perfecto__xmover-right") {
-                  overlay_position_x = overlay_position_x + movestep;
-                  $("#perfecto__imageoverlaycontrols-xmover-input").val(overlay_position_x);
+                  composition_position_x = composition_position_x + movestep;
+                  $("#perfecto__imagecompositioncontrols-xmover-input").val(composition_position_x);
               } else if (this.id=="perfecto__ymover-up") {
-                  overlay_position_y = overlay_position_y - movestep;
-                  $("#perfecto__imageoverlaycontrols-ymover-input").val(overlay_position_y);
+                  composition_position_y = composition_position_y - movestep;
+                  $("#perfecto__imagecompositioncontrols-ymover-input").val(composition_position_y);
               } else if (this.id=="perfecto__ymover-down") {
-                  overlay_position_y = overlay_position_y + movestep;
-                  $("#perfecto__imageoverlaycontrols-ymover-input").val(overlay_position_y);
+                  composition_position_y = composition_position_y + movestep;
+                  $("#perfecto__imagecompositioncontrols-ymover-input").val(composition_position_y);
               }
-              overlay.css({
-                  "top": overlay_position_y+"px",
-                  "left": overlay_position_x +"px"
+              composition.css({
+                  "top": composition_position_y+"px",
+                  "left": composition_position_x +"px"
               });
-              $.cookie("overlay_position_y", overlay_position_y);
-              $.cookie("overlay_position_x", overlay_position_x);
+              $.cookie("composition_position_y", composition_position_y);
+              $.cookie("composition_position_x", composition_position_x);
           });
 
-          imageoverlay_wrap.bind({
+          imagecomposition_wrap.bind({
               mouseenter: function() {
-                  imageoverlaycontrols.css({
+                  imagecompositioncontrols.css({
                       "display": "block"
                   });
               },
               mouseleave: function() {
-                  if (overlay_visible!=="true") {
-                      imageoverlaycontrols.css({
+                  if (composition_visible!=="true") {
+                      imagecompositioncontrols.css({
                           "display": "none"
                       });
                   }
--- perfecto.tpl.php
+++ perfecto.tpl.php
@@ -1,59 +1,59 @@
 <?php
 /**
  * @file
- * Template for overlay navigation.
+ * Template for composition control panel.
  *
  */
 ?>
-<div id="perfecto__imageoverlaycontrols_wrap">
-  <div id="perfecto__imageoverlaycontrols_mousehook"><!-- --></div>
-  <div id="perfecto__imageoverlaycontrols" class="perfecto" style="display: none">
-    <div id="perfecto__imageoverlaycontrols-inner">
+<div id="perfecto__imagecompositioncontrols_wrap">
+  <div id="perfecto__imagecompositioncontrols_mousehook"><!-- --></div>
+  <div id="perfecto__imagecompositioncontrols" class="perfecto" style="display: none">
+    <div id="perfecto__imagecompositioncontrols-inner">
 
-      <div class="perfecto__imageoverlaycontrols-row">
-        <div class="perfecto__imageoverlaycontrols-caption">
-          Overlay
+      <div class="perfecto__imagecompositioncontrols-row">
+        <div class="perfecto__imagecompositioncontrols-caption">
+          Composition
         </div>
-        <select name="" id="perfecto__imageoverlaycontrols-files">
-          <?php foreach ($overlays as $overlay): ?>
-          <option value="<?php print $overlay->filename; ?>" <?php (isset($_COOKIE['overlay_filename']) && $_COOKIE['overlay_filename'] == $overlay->filename)? print 'selected' : '' ?>><?php print $overlay->filename; ?></option>
+        <select name="" id="perfecto__imagecompositioncontrols-files">
+          <?php foreach ($compositions as $composition): ?>
+          <option value="<?php print $composition->filename; ?>" <?php (isset($_COOKIE['composition_filename']) && $_COOKIE['composition_filename'] == $composition->filename)? print 'selected' : '' ?>><?php print $composition->filename; ?></option>
           <?php endforeach; ?>
         </select>
       </div>
 
-      <div class="perfecto__imageoverlaycontrols-row">
-        <div class="perfecto__imageoverlaycontrols-caption">
+      <div class="perfecto__imagecompositioncontrols-row">
+        <div class="perfecto__imagecompositioncontrols-caption">
           Opacity
         </div>
-        <div id="perfecto__imageoverlaycontrols-opacity-slider"></div>
+        <div id="perfecto__imagecompositioncontrols-opacity-slider"></div>
       </div>
 
-      <div class="perfecto__imageoverlaycontrols-row">
-        <div class="perfecto__imageoverlaycontrols-caption">
+      <div class="perfecto__imagecompositioncontrols-row">
+        <div class="perfecto__imagecompositioncontrols-caption">
           Position X
         </div>
-        <div class="perfecto__imageoverlaycontrols-mover perfecto__imageoverlaycontrols-xmover">
-          <div id="perfecto__xmover-left" class="perfecto__imageoverlaycontrols-mover-decrease"></div>
-          <input id="perfecto__imageoverlaycontrols-xmover-input" class="perfecto__imageoverlaycontrols-mover-input" type="text" value="0" />
-          <div id="perfecto__xmover-right" class="perfecto__imageoverlaycontrols-mover-increase"></div>
+        <div class="perfecto__imagecompositioncontrols-mover perfecto__imagecompositioncontrols-xmover">
+          <div id="perfecto__xmover-left" class="perfecto__imagecompositioncontrols-mover-decrease"></div>
+          <input id="perfecto__imagecompositioncontrols-xmover-input" class="perfecto__imagecompositioncontrols-mover-input" type="text" value="0" />
+          <div id="perfecto__xmover-right" class="perfecto__imagecompositioncontrols-mover-increase"></div>
         </div>
       </div>
 
-      <div class="perfecto__imageoverlaycontrols-row">
+      <div class="perfecto__imagecompositioncontrols-row">
 
-        <div class="perfecto__imageoverlaycontrols-caption">
+        <div class="perfecto__imagecompositioncontrols-caption">
           Position Y
         </div>
-        <div class="perfecto__imageoverlaycontrols-mover perfecto__imageoverlaycontrols-ymover">
-          <div id="perfecto__ymover-down" class="perfecto__imageoverlaycontrols-mover-decrease"></div>
-          <input id="perfecto__imageoverlaycontrols-ymover-input" class="perfecto__imageoverlaycontrols-mover-input" type="text" value="0" />
-          <div id="perfecto__ymover-up" class="perfecto__imageoverlaycontrols-mover-increase"></div>
+        <div class="perfecto__imagecompositioncontrols-mover perfecto__imagecompositioncontrols-ymover">
+          <div id="perfecto__ymover-down" class="perfecto__imagecompositioncontrols-mover-decrease"></div>
+          <input id="perfecto__imagecompositioncontrols-ymover-input" class="perfecto__imagecompositioncontrols-mover-input" type="text" value="0" />
+          <div id="perfecto__ymover-up" class="perfecto__imagecompositioncontrols-mover-increase"></div>
         </div>
       </div>
 
-      <div class="perfecto__imageoverlaycontrols-row">
+      <div class="perfecto__imagecompositioncontrols-row">
 
-        <div class="perfecto__imageoverlaycontrols-caption">
+        <div class="perfecto__imagecompositioncontrols-caption">
           Lock?
         </div>
         <div class="c">
@@ -61,9 +61,9 @@
         </div>
       </div>
 
-      <div class="perfecto__imageoverlaycontrols-row-buttons">
-        <a id="perfecto__imageoverlaycontrols_toggle" class="perfecto__imageoverlaycontrols-row-button" href="javascript:void(0)">toggle</a>
-        <a id="perfecto__imageoverlaycontrols_reset" class="perfecto__imageoverlaycontrols-row-button" href="javascript:void(0)">reset</a>
+      <div class="perfecto__imagecompositioncontrols-row-buttons">
+        <a id="perfecto__imagecompositioncontrols_toggle" class="perfecto__imagecompositioncontrols-row-button" href="javascript:void(0)">toggle</a>
+        <a id="perfecto__imagecompositioncontrols_reset" class="perfecto__imagecompositioncontrols-row-button" href="javascript:void(0)">reset</a>
       </div>
     </div>
   </div>
