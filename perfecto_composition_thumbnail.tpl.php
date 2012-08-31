026184dffd733572dc601dd5e1bd4d66d19dc882
Hannes Kirsman <hannes.kirsman@eesti.ee>
2012-08-31 16:56:34 +0300

    Some fixes.
    Some HTML to template.
    " to '

---
+++ perfecto_composition_thumbnail.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Thumbnail HTML for composition to show in admin.
+ */
+?>
+<div style="float:left;margin:10px;">
+  <?php print $image; ?>
+  <div><?php print $filename; ?></div>
+  <div><?php print $view_full_size_link; ?></div>
+  <div><?php print $delete_link; ?></div>
+</div>
