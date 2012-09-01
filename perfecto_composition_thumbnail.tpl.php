e3359ab0a514e2749578a255d909ec893e19baf5
Hannes Kirsman <hkirsman@gmail.com>
2012-09-01 20:08:59 +0300

    Move some CSS, HTML out of .module file

--- perfecto_composition_thumbnail.tpl.php
+++ perfecto_composition_thumbnail.tpl.php
@@ -4,9 +4,16 @@
  * Thumbnail HTML for composition to show in admin.
  */
 ?>
-<div style="float:left;margin:10px;">
-  <?php print $image; ?>
-  <div><?php print $filename; ?></div>
-  <div><?php print $view_full_size_link; ?></div>
-  <div><?php print $delete_link; ?></div>
+<div id="perfecto-thumbnail-list">
+  <?php if($delete_all_link): ?>
+    <p><?php print $delete_all_link; ?></p>
+  <?php endif; ?>
+  <?php foreach($thumbnails as $thumbnail): ?>
+    <div class="thumbnail">
+      <?php print $thumbnail['image']; ?>
+      <div><?php print $thumbnail['filename']; ?></div>
+      <div><?php print $thumbnail['view_full_size_link']; ?></div>
+      <div><?php print $thumbnail['delete_link']; ?></div>
+    </div>
+  <?php endforeach; ?>
 </div>
